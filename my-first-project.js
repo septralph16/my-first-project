c4d1d913769e57adec897b6e2191413c368dfe4a
septralph16 <31354699+septralph16@users.noreply.github.com>
2017-11-18 15:12:11 -0600

    Update my-first-project.js

--- my-first-project.js
+++ my-first-project.js
@@ -1,2 +1,2 @@
-var correct = true
-if (correct{console.log("It's correct!")}else{console.log("It's wrong!")})
+var correct = false
+if // (correct{console.log("It's true!")}else // {console.log("It's false!")})
